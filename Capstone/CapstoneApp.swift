2677d8166c90c317084d6d2f0360182740f009bc
Braydon Whitfield <whitfield17@gmail.com>
2021-09-14 12:22:13 -0400

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Braydon Whitfield on 2021-09-14.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
